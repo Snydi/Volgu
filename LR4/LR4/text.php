3e875a9e94ea3259b00594f2a2e2cac4ba525c52
Snydi <90327066+Snydi@users.noreply.github.com>
2021-11-23 19:35:18 +0300

    Add files via upload

---
+++ LR4/LR4/text.php
@@ -0,0 +1,16 @@
+<?php
+error_reporting(E_ERROR | E_PARSE);
+include_once  'header.php';
+include_once  'preset.php';
+require_once 'logic.php';
+?>
+    <form action="text.php" method="POST"  >
+            <div class="mb-3" >
+                <label>Text</label>
+                <textarea  name="text" placeholder="text" class="form-control"><?php echo $_POST['text']; ?></textarea>
+                <input type="submit" name="submit" value="Войти" class="btn btn-success">
+            </div>
+    </form>
+<?php
+include_once 'footer.php';
+
